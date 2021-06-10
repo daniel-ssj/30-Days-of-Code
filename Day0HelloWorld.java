4d5b6ca32e7cabdd46cd62d079e636573675241d
Daniel Paulino <46933870+daniel-ssj@users.noreply.github.com>
2021-06-10 12:08:06 +0000

    day 7

--- Day0HelloWorld.java
+++ Day0HelloWorld.java
@@ -6,10 +6,10 @@ import java.util.regex.*;
 public class Solution {
 	public static void main(String[] args) {
     // Create a Scanner object to read input from stdin.
-		Scanner scan = new Scanner(System.in); 
+		Scanner scan = new Scanner(System.in);
 		
 		// Read a full line of input from stdin and save it to our variable, inputString.
-		String inputString = scan.nextLine(); 
+		String inputString = scan.nextLine();
 
 		// Close the scanner object, because we've finished reading 
         // all of the input from stdin needed for this challenge.
