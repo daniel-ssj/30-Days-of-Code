5950854b68c388954c8cb79bea1a243bb33799ff
Daniel Paulino <46933870+daniel-ssj@users.noreply.github.com>
2021-06-12 14:13:02 +0000

    day 9

---
+++ Day9Recusion3.java
@@ -0,0 +1,16 @@
+class Result {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+    // Write your code here
+     if (n == 1) return 1;
+        return factorial(n - 1) * n;
+    }
+
+}
